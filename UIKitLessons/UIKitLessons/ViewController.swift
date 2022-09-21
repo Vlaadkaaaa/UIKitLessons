797dfe3df1279e9c5be68ae32838f4d03d382c4e
Vladka <vlad.lymar@list.ru>
2022-09-21 16:46:08 +0500

    Init

---
+++ UIKitLessons/UIKitLessons/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  UIKitLessons
+//
+//  Created by Владислав Лымарь on 21.09.2022.
+//
+
+import UIKit
+
+///
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
